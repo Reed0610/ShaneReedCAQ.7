7f6bef63028abee76e45ab143584ec63d796c19c
Reed0610 <shanereed19@gmail.com>
2023-07-20 10:30:07 +0000

    Commit 2
    Signed-off-by: Reed0610 <shanereed19@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+DigitalOut led1(LED1); // LED1 is connected to pin PA_5
+DigitalOut led2(LED2); // LED2 is connected to pin PB_14
 
+int main() {
+    while (1) {
+        // Toggle LED1 every 2 seconds
+        led1 = !led1;
+        ThisThread::sleep_for(2s);
+
+        // Toggle LED2 every 5 seconds
+        led2 = !led2;
+        ThisThread::sleep_for(5s);
     }
 }
-
